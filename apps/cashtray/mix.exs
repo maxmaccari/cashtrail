8c40c153d32e29d66a115d225f1b3f315f5d8753
Maxsuel Maccari <maxmaccari@hotmail.com>
2020-04-27 20:04:24 -0400

    Remove --quite from ecto.migrate
    https://github.com/maxmaccari/cashtray_backend/issues/1

--- apps/cashtray/mix.exs
+++ apps/cashtray/mix.exs
@@ -59,7 +59,7 @@ defmodule Cashtray.MixProject do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
