9852c12bb943db03e5be76e166a7bf159d08ffdd
Maxsuel Maccari <maxmaccari@hotmail.com>
2020-04-30 18:18:35 -0400

    Install ex_doc to generate project documentation
    https://github.com/maxmaccari/cashtray/issues/21

--- mix.exs
+++ mix.exs
@@ -30,7 +30,8 @@ defmodule Cashtray.Umbrella.MixProject do
     [
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.12.3", only: :test},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
   end
 
