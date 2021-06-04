afd5af96055f32ed9f9055f1dc41187309cd903b
Maxsuel Maccari <maxmaccari@hotmail.com>
2021-06-04 18:18:46 -0400

    Upgrade excoveralls

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule Cashtrail.Umbrella.MixProject do
   defp deps do
     [
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.12.3", only: :test},
+      {:excoveralls, "~> 0.14.1", only: :test},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:benchee, "~> 1.0", only: :dev}
