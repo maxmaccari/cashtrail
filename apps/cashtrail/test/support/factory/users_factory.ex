6374f9cc52262bc216aa235d811444531e8bad97
Maxsuel Maccari <maxmaccari@hotmail.com>
2021-06-04 18:38:22 -0400

    Change deprecated attribute for Faker

--- apps/cashtrail/test/support/factory/users_factory.ex
+++ apps/cashtrail/test/support/factory/users_factory.ex
@@ -14,8 +14,8 @@ defmodule Cashtrail.Factory.UsersFactory do
 
         user = %Users.User{
           email: Faker.Internet.email(),
-          first_name: Faker.Name.first_name(),
-          last_name: Faker.Name.last_name(),
+          first_name: Faker.Person.first_name(),
+          last_name: Faker.Person.last_name(),
           avatar_url:
             "#{Faker.Internet.image_url()}#{Enum.random([".png", ".jpg", ".jpeg", ".gif", ""])}"
         }
